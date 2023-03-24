89a6a3f580d6cd90b77e17f99641552711289199
Abhishek Choudhari <abhishekc97@gmail.com>
2023-03-24 20:51:46 +0530

    changed API paths to use barbecue naming convention, removed comments

--- routes/admin.js
+++ routes/admin.js
@@ -4,68 +4,60 @@ const route = Router();
 const LockPin = require("../models/LockPin");
 
 // create a pin, created only if no pin exists
-route.post("/createPin", async function(req, res, next) {
+route.post("/create-pin", async function (req, res, next) {
     try {
         const inputPin = req.body.pin;
-        if(!inputPin) res.status(400).send("Bad request, check given parameters");
+        if (!inputPin)
+            res.status(400).send("Bad request, check given parameters");
 
         const newPin = {
             pin: inputPin,
         };
 
         const results = await LockPin.find({});
-        console.log(results.length);
 
-        if(results.length === 0) {
+        if (results.length === 0) {
             LockPin.create(newPin);
-            res.status(200).send("New pin created")
-            console.log("New pin created");
-            
+            res.status(200).send("New pin created");
         } else {
             res.status(409).send("Bad request, resource already exists");
         }
-
     } catch (error) {
         console.log(error);
         next(error);
     }
-})
+});
 
 // verify pin entered by user
-route.post("/verifyPin", async function( req, res, next) {
+route.post("/verify-pin", async function (req, res, next) {
     try {
         const inputPin = req.body.pin;
-        if(!inputPin) res.status(400).send("Bad request, check given parameters");
+        if (!inputPin)
+            res.status(400).send("Bad request, check given parameters");
 
-        const results = await LockPin.find({ pin : inputPin});
-        console.log(results.length);
+        const results = await LockPin.find({ pin: inputPin });
 
-        if(results.length === 1) {
-            console.log("pin verified");
+        if (results.length === 1) {
             res.status(200).send("Pin Verification successful");
         } else {
             res.status(401).send("Pin could not be verified");
         }
-
     } catch (error) {
         console.log(error);
         next(error);
     }
-})
+});
 
 // check if a pin exists or not, find pin, status api
-route.get("/pinStatus", async function(req, res, next) {
+route.get("/pin-status", async function (req, res, next) {
     try {
         const results = await LockPin.find({});
-        console.log(results.length);
 
-        if(results.length !== 0) {
-            console.log("pin exists");
+        if (results.length !== 0) {
             res.status(200).send("Pin exists");
         } else {
             res.status(404).send("Pin does not exist");
         }
-
     } catch (error) {
         console.log(error);
         next(error);
@@ -73,22 +65,20 @@ route.get("/pinStatus", async function(req, res, next) {
 });
 
 // modify an existing pin
-route.post("/changePin", async function(req, res, next) {
+route.post("/change-pin", async function (req, res, next) {
     try {
         const inputPin = req.body.pin;
-        if(!inputPin) res.status(400).send("Bad request, check given parameters");
+        if (!inputPin)
+            res.status(400).send("Bad request, check given parameters");
 
         const newPin = {
             pin: inputPin,
         };
 
         const results = await LockPin.find({});
-        console.log(results);
-        console.log(results._id);
-        console.log(results.length);
 
-        if(results.length === 1) {
-            await LockPin.updateOne( {$set: {pin: inputPin}} );
+        if (results.length === 1) {
+            await LockPin.updateOne({ $set: { pin: inputPin } });
             res.status(200).send("Pin updated");
         } else {
             res.status(409).send("Bad request, could not update pin.");
@@ -97,6 +87,6 @@ route.post("/changePin", async function(req, res, next) {
         console.log(error);
         next(error);
     }
-} )
+});
 
 module.exports = route;
--- routes/operations.js
+++ routes/operations.js
@@ -5,10 +5,9 @@ const Folder = require("../models/Folder");
 const File = require("../models/File");
 
 // create a folder
-route.post("/newFolder", async function (req, res, next) {
+route.post("/new-folder", async function (req, res, next) {
     try {
         const folderName = req.body.name;
-        console.log(folderName);
         if (!folderName)
             res.status(400).send("Bad request, check given parameters");
 
@@ -18,7 +17,6 @@ route.post("/newFolder", async function (req, res, next) {
 
         await Folder.create(newFolder);
 
-        console.log("New folder created");
         res.status(200).send("New folder created");
     } catch (error) {
         console.log(error);
@@ -27,22 +25,18 @@ route.post("/newFolder", async function (req, res, next) {
 });
 
 // get all folders
-route.get("/getFolders", async function (req, res, next) {
+route.get("/folders", async function (req, res, next) {
     try {
         const results = await Folder.find({});
-
-        // console.log(results);
-
         res.json(results);
-
     } catch (error) {
         console.log(error);
         next(error);
     }
-})
+});
 
 // create a file with a name
-route.post("/newFile", async function (req, res, next) {
+route.post("/new-file", async function (req, res, next) {
     try {
         const fileName = req.body.name;
         if (!fileName)
@@ -59,7 +53,6 @@ route.post("/newFile", async function (req, res, next) {
 
         await File.create(newFile);
 
-        console.log("New file created");
         res.status(200).send("New file created");
     } catch (error) {
         console.log(error);
@@ -68,31 +61,24 @@ route.post("/newFile", async function (req, res, next) {
 });
 
 // add contents to a file
-route.post("/editFileContent", async function (req, res, next) {
+route.post("/edit-file", async function (req, res, next) {
     try {
         const fileId = req.body.id;
         if (!fileId)
             res.status(400).send("Bad request, check given parameters");
 
-        // const fileName = req.body.filename;
-        // if(!fileName) res.status(400).send("Bad request, check given parameters");
-
         const fileContents = req.body.contents;
-        console.log(fileContents);
 
         const fileFound = await File.findOne({ _id: fileId });
-        console.log(fileFound);
 
         if (fileFound) {
             await File.updateOne(
                 { _id: fileId },
                 { $set: { contents: fileContents } }
             );
-            console.log("Updated the file contents");
-            // res.send("Updated the file contents");
+            res.status(200).send("Updated the file contents");
         } else {
-            console.log("could not find file to update contents");
-            // res.send("could not find file to update contents");
+            res.status(200).send("could not find file to update contents");
         }
     } catch (error) {
         console.log(error);
@@ -101,17 +87,14 @@ route.post("/editFileContent", async function (req, res, next) {
 });
 
 // api to get all files in a particular folder
-route.get("/getFiles/:folder", async function (req, res, next) {
+route.get("/folder/:folder/files", async function (req, res, next) {
     try {
         const folderName = req.params.folder;
         if (!folderName)
             res.status(400).send("Bad request, check given parameters");
 
         const results = await File.find({ folder: { $in: [folderName] } });
-
-        // console.log(results);
         res.json(results);
-
     } catch (error) {
         console.log(error);
         next(error);
@@ -119,16 +102,14 @@ route.get("/getFiles/:folder", async function (req, res, next) {
 });
 
 // api to get a particular file with its id
-route.get("/getFile/:fileId", async function (req, res, next) {
+route.get("/file/:fileId", async function (req, res, next) {
     try {
         const fileId = req.params.fileId;
-        if(!fileId) res.status(400).send("Bad request, check given parameters");
+        if (!fileId)
+            res.status(400).send("Bad request, check given parameters");
 
-        const results = await File.find({ _id: fileId});
-
-        // console.log(results);
+        const results = await File.find({ _id: fileId });
         res.json(results);
-
     } catch (error) {
         console.log(error);
         next(error);
@@ -136,12 +117,11 @@ route.get("/getFile/:fileId", async function (req, res, next) {
 });
 
 // get all files from files collection
-route.get("/getAllFiles", async function (req, res, next) {
+route.get("/files", async function (req, res, next) {
     try {
         const results = await File.find({});
 
-        if(results) res.json(results);
-
+        if (results) res.json(results);
     } catch (error) {
         console.log(error);
         next(error);
